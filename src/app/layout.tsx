39da79e33392219b0060db75a43b61dc0f5dddd2
moscowmule7 <oguzsamsa@gmail.com>
2024-11-04 12:21:23 +0300

    auth completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import { Providers } from "../components/providers"
-import { EB_Garamond } from "next/font/google"
-import { cn } from "@/utils"
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { Providers } from '../components/providers'
+import { EB_Garamond } from 'next/font/google'
+import { cn } from '@/utils'
 
-import "./globals.css"
-import { ClerkProvider } from "@clerk/nextjs"
+import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 const eb_garamond = EB_Garamond({
-  subsets: ["latin"],
-  variable: "--font-heading",
+  subsets: ['latin'],
+  variable: '--font-heading',
 })
 
 export const metadata: Metadata = {
-  title: "jStack App",
-  description: "Created using jStack",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  title: 'jStack App',
+  description: 'Created using jStack',
+  icons: [{ rel: 'icon', url: '/favicon.ico' }],
 }
 
 export default function RootLayout({
@@ -26,12 +26,13 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en" className={cn(inter.variable, eb_garamond.variable)}>
-      <body className="font-sans bg-brand-50 text-brand-950 antialiased">
-        <Providers>
-          {children}</Providers>
-      </body>
-    </html>
+      <html lang="en" className={cn(inter.variable, eb_garamond.variable)}>
+        <body className="min-h-[calc(100vh-1px)] flex flex-col font-sans bg-brand-50 text-brand-950 antialiased">
+          <main className="relative flex-1 flex flex-col">
+            <Providers>{children}</Providers>
+          </main>
+        </body>
+      </html>
     </ClerkProvider>
   )
 }
